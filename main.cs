4bcc890af330765f9b24ce959d4f0dc29238de40
Andregynouz <158660413+Andregynouz@users.noreply.github.com>
2024-02-17 11:32:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string input = "";
+    string output = "";
+
+    do
+    {
+      Console.WriteLine ("Enter a Word!:");
+      input = Console.ReadLine();
+      if (input != "exit")
+      {
+        output += input + " ";
+        Console.WriteLine(output);
+      }
+    } while (input != "exit");
+
+    Console.WriteLine($"Closing program...");
+  }
+}
